b49297829c233aef5b79514b4fe11753a56078a4
Seren <moyan548@foxmail.com>
2026-05-10 23:12:35 +0800

    修改配置

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tailwindcss()
+  ],
+  server: {
+    port: 5173,
+    proxy: {
+      // proxy 只在开发时用，解决跨域，配置一次，永远不用管，是前后端接口通信的问题，可以正常通信
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      },
+      '/uploads': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  }
 })
